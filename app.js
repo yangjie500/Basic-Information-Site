6271a0943e9e525a154be85633880a2409d71d95
Yang Jie <kristeh97@.gmail.com>
2021-12-24 18:44:10 +0800

    First Node Project

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(3000);
+
+app.use(express.static("public"))
+
+app.get('/', (req, res) => {
+  res.sendFile('./views/index.html', { root: __dirname })
+})
+
+app.get('/about', (req, res) => {
+  res.sendFile('./views/about.html', { root: __dirname })
+})
+
+app.get('/contact', (req, res) => {
+  res.sendFile('./views/contact-me.html', { root: __dirname })
+})
+
+app.use((req, res) => {
+  res.status(404).sendFile('./views/404.html', { root: __dirname })
+})
